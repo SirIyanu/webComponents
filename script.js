f71eb994ea7ee91ba499aab53a305757dc26881a
SirIyanu <105286160+SirIyanu@users.noreply.github.com>
2024-02-11 19:15:06 +0100

    fixed the error in the console

--- script.js
+++ script.js
@@ -1,52 +1,90 @@
-class RootComponent extends HTMLElement {
-    name ='root'
-    constructor( ) {
-        super()
-        this.shadow = this.attachShadow({mode: 'open'})
-        // shadow.innerHTML = `TGESHH`
-    }
+// class RootComponent extends HTMLElement {
+//     name ='root'
+//     constructor( ) {
+//         super()
+//         this.shadow = this.attachShadow({mode: 'open'})
+//         // shadow.innerHTML = `TGESHH`
+//     }
 
-    static get observedAttributes(){
-        return ['info']
-    }
+//     static get observedAttributes(){
+//         return ['info']
+//     }
 
-}
-
-// class HiddenInfo extends RootComponent{
-// constructor(){
-//     super()
 // }
 
+// // class HiddenInfo extends RootComponent{
+// // constructor(){
+// //     super()
+// // }
 
 
+
+
+// // connectedCallback(){
+// //     this.shadow.innerHTML = `
+// //     <div class="pluswrapper">
+// //     <button class="plus">+</button>
+// //     <p class="info hidden"></p>
+// //   </div>
+// //     `
+// // }
+// // }
+
+// class Pluswrapper extends RootComponent {
+// name ='plus'
+// constructor(){
+//     super()
+//     this.shadow = this.attachShadow({mode: 'open'})
+   
+// }
 
 // connectedCallback(){
-//     this.shadow.innerHTML = `
+//     const info = this.getAttribute('info')
+//     this.shadow.innerHTML =`
 //     <div class="pluswrapper">
-//     <button class="plus">+</button>
-//     <p class="info hidden"></p>
+//  <hidden-info title=${info}></hidden-info>
 //   </div>
 //     `
 // }
 // }
 
-class Pluswrapper extends RootComponent {
-name ='plus'
-constructor(){
-    super()
-    this.shadow = this.attachShadow({mode: 'open'})
-   
+// customElements.define('hidden-info', HiddenInfo)
+// customElements.define('plus-component', Pluswrapper)
+
+
+class RootComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: 'open' });
+    }
+
+    static get observedAttributes() {
+        return ['info'];
+    }
+
+    connectedCallback() {
+        const info = this.getAttribute('info');
+        this.shadow.innerHTML = `
+            <div class="pluswrapper">
+                <hidden-info title="${info}"></hidden-info>
+            </div>
+        `;
+    }
 }
 
-connectedCallback(){
-    const info = this.getAttribute('info')
-    this.shadow.innerHTML =`
-    <div class="pluswrapper">
- <hidden-info title=${info}></hidden-info>
-  </div>
-    `
-}
+class HiddenInfo extends HTMLElement {
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        const title = this.getAttribute('title');
+        this.shadow.innerHTML = `
+            <p class="info hidden">${title}</p>
+        `;
+    }
 }
 
-customElements.define('hidden-info', HiddenInfo)
-customElements.define('plus-component', Pluswrapper)
+customElements.define('root-component', RootComponent);
+customElements.define('hidden-info', HiddenInfo);
